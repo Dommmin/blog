4ac455d542de5dd7b350408b099114dffa850d2e
Dominik <d.jasinski@ideo.pl>
2024-03-05 15:10:47 +0100

    feat: update docker

--- backend/.tools/scripts/startup.sh
+++ backend/.tools/scripts/startup.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
 
-#dockerize -wait tcp://db:3306 -timeout 60s
-
 #php artisan queue:work
 #php artisan queue:work redis --queue=scout
-php artisan migrate
-php artisan serve --host=0.0.0.0 --port=8000
+#composer install --no-dev --optimize-autoloader --no-scripts
+
+mkdir -p storage/framework/{sessions,views,cache}
+php artisan storage:link
+php artisan optimize:clear
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+php artisan event:cache
+php artisan migrate --force
+php artisan key:generate
+
+php artisan octane:start --watch --workers=10 --host=0.0.0.0 --port=8000
+#php artisan serve --host=0.0.0.0 --port=8000
