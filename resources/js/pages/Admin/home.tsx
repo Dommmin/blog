53be5a4e21763439db52b8ec496bbd230ce482a9
Dominik <domijas96@gmail.com>
2025-05-06 11:32:01 +0200

    [Fix] Remove unexisting prop

--- resources/js/pages/Admin/home.tsx
+++ resources/js/pages/Admin/home.tsx
@@ -1,19 +1,11 @@
 import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
 import { Toaster } from '@/components/ui/sonner';
 import AdminLayout from '@/layouts/admin-layout';
-import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/react';
-
-const breadcrumbs: BreadcrumbItem[] = [
-    // {
-    //     title: 'Home',
-    //     href: '/dashboard',
-    // },
-];
 
 export default function Home() {
     return (
-        <AdminLayout breadcrumbs={breadcrumbs}>
+        <AdminLayout>
             <Toaster />
             <Head title="Home" />
             <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
