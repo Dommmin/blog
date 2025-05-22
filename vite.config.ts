9fc6f55a6f1497150ac01af5449332bf0d55fc48
Dominik <93583747+Dommmin@users.noreply.github.com>
2025-05-23 01:27:31 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -106,7 +106,7 @@ export default defineConfig({
         },
     },
     ssr: {
-        noExternal: true,
+        noExternal: ['@inertiajs/react'],
     },
     server: {
         host: '0.0.0.0',
